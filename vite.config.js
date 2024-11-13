56ea99d3ee053f3c86c7fba2d85206ab90bad1a9
engmung <122682380+engmung@users.noreply.github.com>
2024-11-14 03:16:45 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 3000
+  },
+  build: {
+    chunkSizeWarningLimit: 1600, // 청크 사이즈 경고 제한 증가
+  },
+  preview: {
+    host: true, // 또는 '0.0.0.0'
+    port: 3000, // 3000 포트로 변경
+    strictPort: true,
   }
 })
